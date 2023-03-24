b97a4e12f9bddac81eea899a3ca5d396a8965012
ianK-code <kian99564@gmail.com>
2023-03-24 13:43:39 +0300

    Post functionality successfully done

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,8 +1,34 @@
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function Create() {
+  const navigate = useNavigate()
+
+  const [todo, setTodo] = useState("");
+  const [status, setStatus] = useState("");
+
   const CreateTodo = (e) => {
     e.preventDefault();
+    const body = {
+      todo: todo,
+      completed: status
+    };
+
+    fetch("http://localhost:8000/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    }).then((res) => {
+      if (!res.ok) {
+        console.log(res.status);
+      }
+      navigate("/home");
+      res.json();
+    });
+
+    console.log(body)
   };
   return (
     <div>
@@ -11,11 +37,23 @@ function Create() {
 
       <form onSubmit={CreateTodo}>
         <label>
-          Name: <input type="text" autoComplete="off" />
+          Todo:{" "}
+          <input
+            onChange={(e) => setTodo(e.target.value)}
+            type="text"
+            autoComplete="off"
+            value={todo}
+          />
         </label>
 
         <label>
-          Description: <input autoComplete="off" type="text" />
+          Status:{" "}
+          <input
+            onChange={(e) => setStatus(e.target.value)}
+            autoComplete="off"
+            type="text"
+            value={status}
+          />
         </label>
 
         <input
@@ -24,8 +62,6 @@ function Create() {
           style={{ cursor: "pointer" }}
         />
       </form>
-
-      
     </div>
   );
 }
